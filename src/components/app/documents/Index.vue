b0374003cc96f8ddd72fc19d841746946feeacd4
Caleb Nasio <nascal3@gmail.com>
2022-04-15 00:13:54 +0300

    Added UI functionality for viewing submitted documents

--- src/components/app/documents/Index.vue
+++ src/components/app/documents/Index.vue
@@ -16,67 +16,8 @@
       <v-row class="mt-5 mb-1">
         <v-col md="6" cols="12">
           <v-row class="date-filters">
-            <v-col cols="12" md="4">
-              <v-menu
-                  ref="menu"
-                  v-model="menu"
-                  :close-on-content-click="false"
-                  :return-value.sync="dateFrom"
-                  :nudge-right="40"
-                  transition="scale-transition"
-                  offset-y
-                  min-width="290px"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                      v-model="dateFrom"
-                      :label="$t('core.from_date')"
-                      prepend-inner-icon="event"
-                      outlined
-                      dense
-                      readonly
-                      v-bind="attrs"
-                      v-on="on"
-                      @blur="loadStatement"
-                  ></v-text-field>
-                </template>
-                <v-date-picker v-model="dateFrom" :locale="locale" :show-current="dateTo" :max="maximumDate">
-                  <v-spacer></v-spacer>
-                  <v-btn class="btn-text" text color="primary" @click="menu = false">{{ $t('orders.cancel') }}</v-btn>
-                  <v-btn class="btn-text" color="primary" @click="$refs.menu.save(dateFrom)">{{ $t('orders.ok') }}</v-btn>
-                </v-date-picker>
-              </v-menu>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-menu
-                  ref="menu2"
-                  v-model="menu2"
-                  :close-on-content-click="false"
-                  :return-value.sync="dateTo"
-                  :nudge-right="40"
-                  transition="scale-transition"
-                  offset-y
-                  min-width="290px"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                      v-model="dateTo"
-                      :label="$t('core.to_date')"
-                      prepend-inner-icon="event"
-                      outlined
-                      dense
-                      readonly
-                      v-bind="attrs"
-                      v-on="on"
-                      @blur="loadStatement"
-                  ></v-text-field>
-                </template>
-                <v-date-picker v-model="dateTo" :locale="locale" :show-current="dateFrom" :min="minimumDate">
-                  <v-spacer></v-spacer>
-                  <v-btn class="btn-text" text color="primary" @click="menu2 = false">{{ $t('orders.cancel') }}</v-btn>
-                  <v-btn class="btn-text" color="primary" @click="$refs.menu2.save(dateTo)">{{ $t('orders.ok') }}</v-btn>
-                </v-date-picker>
-              </v-menu>
+            <v-col cols="12" md="7">
+              <date-range @getDateRange="setDateRange"/>
             </v-col>
             <v-col cols="12" md="4">
               <v-select
@@ -87,7 +28,7 @@
                   item-value="paymentValue"
                   dense
                   outlined
-                  @change="loadStatement"
+                  @change="loadDocuments"
               ></v-select>
             </v-col>
           </v-row>
@@ -98,7 +39,7 @@
 
       <v-card-text class="px-0">
         <v-data-table
-            id="statement-table"
+            id="documents-table"
             fixed-header
             disable-sort
             class="title"
@@ -107,15 +48,14 @@
             :no-data-text="$t('documents.no_documents_found')"
             :no-results-text="$t('documents.no_results_found')"
             :headers="headers"
-            :items="statement"
+            :items="documents"
             item-key="txn_no"
             :loading-text="$t('core.system_loading')"
         >
         </v-data-table>
 
         <app-pagination
-            v-if="statement.length"
-            id="statement-pagination"
+            v-if="documents.length"
             :meta="meta"
             @pageChanged="pageChanged"
         />
@@ -126,40 +66,29 @@
 
 <script>
 import { format } from 'date-fns'
-import Order from '@/libs/app/orders/Order'
-import OrderDetails from '@/libs/app/order_details/OrderDetails'
-import User from '@/libs/app/users/User'
+import Document from '@/libs/app/orders/Order'
 
 export default {
+  components: {
+    'date-range': () => import('@/components/core/DateRange.vue'),
+  },
+
   data () {
     return {
       loading: true,
-      orderObj: new Order(),
-      orderDetailsObj: new OrderDetails(),
-      usersObj: new User(),
-      statement: [],
-      orderDetails: {},
-      orderDetailsError: {},
+      documentObj: new Document(),
+      documents: [],
       selectedDocumentStatus: {paymentLabel: this.$t('documents.status_all'), paymentValue: 'all'},
       documentStatus: [
         {paymentLabel: this.$t('documents.status_all'), paymentValue: 'all'},
         {paymentLabel: this.$t('documents.status_pending'), paymentValue: 'pending'},
         {paymentLabel: this.$t('documents.status_approved'), paymentValue: 'approved'}
       ],
-      menu: false,
-      menu2: false,
-      dateFrom: new Date().toISOString().substr(0, 10),
-      dateTo: new Date().toISOString().substr(0, 10),
-      minimumDate: '',
-      maximumDate: '',
-      locale: localStorage.getItem('setLanguage'),
       page: 1,
       headers: [
-        { text: this.$t('finance.txn'), value: 'order_no' },
-        { text: this.$t('finance.txn_date'), value: 'pickup_location' },
-        { text: this.$t('finance.txn_amount'), value: 'destinations' },
-        { text: this.$t('finance.txn_balance'), value: 'distance' },
-        { text: this.$t('finance.txn_driver'), value: 'updated_at' }
+        { text: this.$t('documents.document_name'), value: 'order_no' },
+        { text: this.$t('documents.submission_date'), value: 'pickup_location' },
+        { text: this.$t('documents.document_status'), value: 'destinations' }
       ],
       meta: {
         current_page: 1,
@@ -175,77 +104,28 @@ export default {
     }
   },
 
-  watch: {
-    dateFrom (newDate) {
-      this.minimumDate = newDate
-    },
-
-    dateTo (newDate) {
-      this.maximumDate = newDate
-    }
-  },
-
   methods: {
-    getOrderDetails ({item, value}) {
-      if (!value) return
-      const { order_no } = item
-      this.orderDetailsObj.show(order_no).then( data => {
-        this.orderDetails = data.data
-      }).catch(error => {
-        this.orderDetailsError = error
-        throw error.data
-      })
-    },
-
-    getDriverIds () {
-      return this.usersObj.show('?roles=driver').then(data => {
-        const drivers = data.data
-        return drivers.map(driver => {
-          return driver.id
-        })
-      }).catch(error => {
-        this.loading = false
-        throw error.data
-      })
-    },
-
     formatDate(date) {
       if (!date) return
       return format(new Date(date), 'iii, do LLL')
     },
 
+    setDateRange({dateFrom, dateTo}) {
+      this.dateFrom = dateFrom
+      this.dateTo = dateTo
+    },
+
     pageChanged (page) {
       this.page = page
-      this.loadStatement()
+      this.loadDocuments()
     },
 
-    formatOrders(ordersData) {
-      if (!ordersData.length) return []
-      let allOrders = []
-      ordersData.forEach(order => {
-        allOrders = [...allOrders, ...order.transporter_orders]
-      })
-
-      return allOrders.sort((a, b) => {
-        return new Date(b.date) - new Date(a.date);
-      });
-    },
-
-    loadStatement () {
+    loadDocuments () {
       this.loading = true
-      this.getDriverIds().then(driverIds => {
-        this.orderObj.show(this.dateFrom, this.dateTo, this.page, driverIds).then(({ data }) => {
-          this.orders = this.formatOrders(data)
-          this.meta.total = this.orders.length
-          this.loading = false
-        }).catch(error => {
-          this.loading = false
-          flash({
-            message: error.data.message,
-            color: '#e74c3c',
-          })
-          throw error
-        })
+      this.documentObj.show(this.dateFrom, this.dateTo, this.page, this.selectedDocumentStatus.paymentValue).then(({ data }) => {
+        this.documents = data
+        this.meta.total = this.documents.length
+        this.loading = false
       }).catch(error => {
         this.loading = false
         flash({
@@ -258,14 +138,14 @@ export default {
   },
 
   mounted () {
-    this.loadStatement()
+    this.loadDocuments()
   }
 
 }
 </script>
 
 <style lang="scss">
-#statement-table {
+#documents-table {
   .v-data-table__wrapper {
     max-height: 90vh;
     overflow-x: hidden;
@@ -304,24 +184,6 @@ export default {
       &:first-letter {
         text-transform: uppercase;
       }
-    }
-  }
-}
-#statement-pagination {
-  .v-pagination {
-    justify-content: flex-end;
-    padding-right: 20px;
-    .v-pagination__navigation {
-      box-shadow: none;
-    }
-    .v-pagination__item--active {
-      color: #324BA8;
-      background-color: transparent !important;
-    }
-    .v-pagination__item {
-      background: transparent;
-      font-weight: 700;
-      box-shadow: none;
     }
   }
 }
