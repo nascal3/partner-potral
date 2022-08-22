e6833f2dc07f37b497c61651e95d970dc802b0b5
Caleb Nasio <nascal3@gmail.com>
2022-08-22 15:13:49 +0300

    Remove had coded date for test

--- src/mixins/dateFormat.js
+++ src/mixins/dateFormat.js
@@ -25,10 +25,10 @@ const dateFormat = {
         withdrawalDateFormat(date, separator) {
             // format date result e.g Saturday from 8am
             if (!date) return '...'
-            const day = format(new Date('2022-08-17'), 'iiii', {
+            const day = format(new Date(date), 'iiii', {
                 locale: locales[setLanguage]
             })
-            const time = format(new Date('2022-08-17'), 'h aaa', {
+            const time = format(new Date(date), 'h aaa', {
                 locale: locales[setLanguage]
             })
             return `${day} ${separator} ${time}`
