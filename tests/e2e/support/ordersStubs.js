f5efa2207e6007b48731f20a2eb2444c1df5bb5f
Caleb Nasio <nascal3@gmail.com>
2022-08-30 17:16:14 +0300

    Added order details display test

--- tests/e2e/support/ordersStubs.js
+++ tests/e2e/support/ordersStubs.js
@@ -1,4 +1,5 @@
 import orders from "../fixtures/orders.json";
+import orderDetails from "../fixtures/orderDetails.json";
 import drivers from "../fixtures/drivers.json";
 import constants from "../fixtures/constants.json";
 
@@ -13,4 +14,9 @@ Cypress.Commands.add('ordersStubs', () => {
         statusCode: 200,
         body: orders
     }).as('orders')
+
+    cy.intercept('GET', `${constants.APIbaseUrl}/partners/141/orders/AJ74G4773-NJM`, {
+        statusCode: 200,
+        body: orderDetails
+    }).as('ordersDetails')
 })
