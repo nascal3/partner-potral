27380b46debc3ae7aceeab8fcb65ff1977933176
Caleb Nasio <nascal3@gmail.com>
2022-09-07 17:47:56 +0300

    Grammatical correction changes

--- src/components/app/legal_documents/AddDocuments.vue
+++ src/components/app/legal_documents/AddDocuments.vue
@@ -139,7 +139,7 @@ export default {
     setCrumbs() {
       const { legal_entity_type } = this.partnerProps
       const pageName = legal_entity_type === 'individual'
-          ? this.$t('documents.add_required_documents') : this.$t('documents.add_business_documents')
+          ? this.$t('documents.required_documents') : this.$t('documents.business_documents')
       this.crumbs.push({ text: pageName , disabled: true })
     },
 
