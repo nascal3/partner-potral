cb78c55e83df7afc23b00deeea1b6aa518478858
Caleb Nasio <nascal3@gmail.com>
2022-10-13 14:38:48 +0300

    Added mixpanel to the events collection process

--- src/mixins/segmentEvents.js
+++ src/mixins/segmentEvents.js
@@ -1,21 +1,36 @@
 const segmentMixin = {
+    computed: {
+        userIdentity() {
+            return  auth.retrieve('user')
+        }
+    },
 
     methods: {
         setSegmentIdentity({user}) {
-            if (process.env.DOCKER_ENV !== 'production') return
+            if (process.env.DOCKER_ENV === 'development') return
             const { id, email, name, phone } = user
             analytics.identify(id, {
                 name,
                 email,
                 phone
-            });
+            })
         },
 
         setSegmentEvent(eventName) {
-            if (process.env.DOCKER_ENV !== 'production') return
+            if (process.env.DOCKER_ENV === 'development') return
             analytics.track(eventName, {
-                event: eventName,
+                event: eventName
             });
+
+            if (!this.userIdentity) return
+            const { id, email, name, phone } = this.userIdentity
+            this.$mixpanel.track(eventName, {
+                distinct_id: id,
+                event: eventName,
+                name,
+                email,
+                phone
+            })
         },
     },
 };
