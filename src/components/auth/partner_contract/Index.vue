f2a4a4c40dae37be2e3963d13f2a11df1ef2475a
Caleb Nasio <nascal3@gmail.com>
2022-11-16 09:54:24 +0300

    redirect if from contract page if its already signed

--- src/components/auth/partner_contract/Index.vue
+++ src/components/auth/partner_contract/Index.vue
@@ -5,7 +5,7 @@
       <v-card-subtitle v-if="rendering" class="mt -6 mb-10 pa-0"> {{ $t('auth.contract_loading') }}</v-card-subtitle>
       <v-card-subtitle v-else class="mt-6 mb-10 pa-0"> {{ $t('auth.contract_subtitle') }}</v-card-subtitle>
 
-      <v-alert  class="mt-5" v-if="!hasPendingContract && !rendering" type="success">{{ $t('documents.contract_signed') }}</v-alert>
+      <v-alert  class="mt-5" v-if="!hasPendingContract" type="success">{{ $t('documents.contract_signed') }}</v-alert>
       <v-card-text v-if="hasPendingContract">
         <vue-pdf-embed
             ref="pdfRef"
@@ -104,11 +104,7 @@ export default {
 
     contractUrl () {
       if (!this.initialised) return 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf'
-
-      if (!this.hasPendingContract) {
-        this.rendering = false
-        return this.$router.push({name: 'partner-contract.index'})
-      }
+      if (!this.hasPendingContract) return this.$router.push({name: 'partner-contract.index'})
 
       const { contracts } = this.pendingContracts.data
       return contracts[contracts.length - 1].contract
