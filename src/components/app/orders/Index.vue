8a441edb0bd2d98374cbdd6fd46d0282a1ff9f12
jeremy <jeremywambugu@sendyit.com>
2023-01-10 10:43:22 +0300

    add minimal styling changes

--- src/components/app/orders/Index.vue
+++ src/components/app/orders/Index.vue
@@ -22,26 +22,24 @@
               @change="setSegmentEvent('Searched an order phrase')"></v-text-field>
         </div>
       </v-card-title>
-
-      <v-divider></v-divider>
-
-      <!--      ##### Required/expired documents message #####-->
-      <v-dialog v-for="(document,index) in notificationDocuments" v-if="document.length" :key="index"
+      <v-dialog v-for="(document,index) in notificationDocuments" v-if="document.length > 0" :key="index"
                 max-width="600" transition="dialog-top-transition">
         <template v-slot:activator="{ on, attrs }">
           <v-card
-              class="d-flex justify-space-between align-center px-8 py-2 mt-3 required-documents"
-              max-width="550" flat>
+              class="d-flex justify-space-between align-center px-8 py-2 mb-3 "
+              elevation="1"
+              tile
+              width="480">
             <div class="d-flex align-center">
               <v-icon color="error" x-large>mdi-alert-circle</v-icon>
               <v-card-text>
                 <p class="body-2 font-weight-bold pa-0 ma-0 pt-4">
-                  {{ index === "expired" ? $t('orders.expiring_document') : $t('orders.new_document_required') }}</p>
-                <p>{{ index === "expired" ? $t('orders.upload', { expiry: topExpiryDate }) : $t('orders.last_day', { deadline: topPendingDate }) }}</p>
+                  {{ index === "expired" ? "Expiring Document" : "New Document Required" }}</p>
+                <p>{{ index === "expired" ? "Upload by:" + topExpiryDate : "Deadline:" + topPendingDate }}</p>
               </v-card-text>
             </div>
-            <v-btn class="text-capitalize" color="error" v-bind="attrs" v-on="on">
-              {{ $t('orders.more_details') }}
+            <v-btn class="text-capitalize font-weight-medium" color="error" v-bind="attrs" v-on="on">
+              More Details
             </v-btn>
           </v-card>
         </template>
@@ -50,33 +48,35 @@
             <div class="d-flex align-center">
               <v-icon class="pr-4" color="error" x-large>mdi-alert-circle</v-icon>
               <v-card-title class="text-h6 pa-0 font-weight-bold">{{
-                  index === "expired" ? $t('orders.expiring_document') : $t('orders.new_document_required')
+                  index === "expired" ? "Expiring Document" : "New Documents Required"
                 }}
               </v-card-title>
             </div>
             <v-card-text>
               <p class="body-1 black--text pt-4">{{ partnerName }}</p>
               <p v-if="index === 'expired'" class="body-1 black--text">
-                {{ $t('orders.expire_message') }}
+                The following documents will be expiring soon:
               </p>
               <div v-else>
                 <p class="body-1 font-weight-medium black--text">
-                  {{ $t('orders.explanation_message') }}
-                </p>
-                <p class="body-2 black--text">{{ $t('orders.reason_message') }}</p>
+                  To offer you, our partner, and customers the best service possible, we need to ensure that we have the
+                  right people and vehicle types servicing orders.</p>
+                <p class="body-2 black--text">We therefore need you to provide the following for verification:</p>
               </div>
               <div v-for="(doc,index) in document" class="d-flex align-center py-2">
                 <v-icon class="pr-5" color="success"
                 >mdi-check-circle-outline
-                </v-icon>
+                </v-icon
+                >
                 <div class="pb-2">
                   <p class="body-2 black--text ma-0">{{ doc.name }}</p>
-                  <p class="body-2 black--text ma-0">{{ $t('orders.expiry:', { date: doc.date }) }}</p>
+                  <p class="body-2 black--text ma-0">Expiry: {{ doc.date }}</p>
                 </div>
               </div>
               <p class="body-1 black--text">
-                {{ $t('orders.please') }} {{ index === "expired" ? $t('orders.renew_them') : "" }} {{ $t('orders.submit_before') }}
-                {{ index === "expired" ? topExpiryDate : topPendingDate }} {{ $t('orders.otherwise') }}
+                Please {{ index === "expired" ? "renew them and" : "" }} submit before
+                {{ index === "expired" ? topExpiryDate : topPendingDate }} otherwise you
+                will not be able to service Sendy orders.
               </p>
             </v-card-text>
             <v-card-actions class="justify-end d-flex flex-column">
@@ -84,21 +84,23 @@
                   class="body-2 px-14 py-5 my-4 text-capitalize"
                   text
                   @click="dialog.value = false"
+              >Remind me later
+              </v-btn
               >
-                {{ $t('orders.remind_me_later') }}
-              </v-btn>
               <v-btn
                   class="body-2 px-14 py-5 text-capitalize"
                   color="primary"
                   large
                   @click="redirectToUpload"
+              >Upload Document
+              </v-btn
               >
-                {{ $t('orders.upload_document') }}
-              </v-btn>
             </v-card-actions>
           </v-card>
         </template>
       </v-dialog>
+
+      <v-divider></v-divider>
 
       <v-row class="mt-5 mb-1">
         <v-col cols="12" md="6">
@@ -319,17 +321,16 @@ export default {
     redirectToUpload() {
       this.$router.push('/legal-documents')
     },
-
     getExpiringDocuments() {
       this.LegalDocumentObj.fetchAll().then(res => {
         const oneMonthFromNow = new Date();
         oneMonthFromNow.setMonth(oneMonthFromNow.getMonth() + 1);
         for (const obj of res.data) {
           if (obj.expires_at < oneMonthFromNow) {
-            if (obj.expires_at !== null) {
+            if (!obj.expires_at === null) {
               this.notificationDocuments.expired.push({
                 name: obj.document.label,
-                date: this.documentsDateFormat(obj.expires_at)
+                date: obj.expires_at
               })
               this.topExpiryDate = this.notificationDocuments.expired[0].date
             }
@@ -338,7 +339,6 @@ export default {
         this.getPendingDocuments()
       })
     },
-
     getPendingDocuments() {
       let pendingDocuments = []
       this.LegalDocumentObj.fetchAll().then(res => {
@@ -347,14 +347,13 @@ export default {
             const date = new Date(obj.created_at) // specific date
             this.notificationDocuments.pending.push({
               name: obj.document.label,
-              date: this.documentsDateFormat( new Date(date.setMonth(date.getMonth() + 1)) )// add one month
+              date: new Date(date.setMonth(date.getMonth() + 1)).toLocaleDateString() // add one month
             })
             this.topPendingDate = this.notificationDocuments.pending[0].date
           }
         }
       })
     },
-
     setDateRange({dateFrom, dateTo}) {
       this.setSegmentEvent("Filter Order/Errand Date");
       this.dateFrom = dateFrom;
@@ -500,8 +499,8 @@ export default {
     },
 
     loadErrands() {
-      this.loadingErrands = true
-      const {id} = auth.retrieve("partner")
+      this.loadingErrands = true;
+      const {id} = auth.retrieve("partner");
       this.getDriverIds()
           .then((driverIds) => {
             this.setErrands({
@@ -514,7 +513,11 @@ export default {
                 drivers: driverIds,
               },
             })
+                .then(() => {
+                  this.loadingErrands = false;
+                })
                 .catch((error) => {
+                  this.loadingErrands = false;
                   flash({
                     message: error.response.data.message,
                     color: "#e74c3c",
@@ -528,9 +531,7 @@ export default {
               color: "#e74c3c",
             });
             throw error;
-          }).finally(() => {
-              this.loadingErrands = false
-          })
+          });
     },
   },
 
@@ -647,10 +648,6 @@ export default {
 }
 </style>
 <style lang="scss" scoped>
-.required-documents {
-  border: solid 1px #FF6160;
-}
-
 .v-tabs {
   //margin-top: 20px;
 }
